13295d635d271bb5e4d04b9b548ee105b009c072
YS L <henry.5a1888@gmail.com>
2020-01-14 10:26:02 +0800

    Initial commit

---
+++ Font Loader/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Font_Loader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load font";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Unload font";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Refresh font list";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 301);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Font Loader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Font Loader/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+
+namespace Font_Loader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+        [DllImport("gdi32.dll", EntryPoint = "AddFontResourceW", SetLastError = true)]
+        public static extern int AddFontResource([In][MarshalAs(UnmanagedType.LPWStr)] string lpFileName);
+        [DllImport("gdi32.dll", EntryPoint = "RemoveFontResourceW", SetLastError = true)]
+        public static extern int RemoveFontResource([In][MarshalAs(UnmanagedType.LPWStr)] string lpFileName);
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            InstalledFontCollection ifc = new InstalledFontCollection();
+
+            listBox1.Items.AddRange(ifc.Families.Select(f => f.Name).ToArray());
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ask = new OpenFileDialog();
+            if (ask.ShowDialog() == DialogResult.OK)
+            {
+                string filename = ask.FileName;
+
+                const int WM_FONTCHANGE = 0x001D;
+                const int HWND_BROADCAST = 0xffff;
+
+                int res = AddFontResource(filename);
+                SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
+                if (res == 0) MessageBox.Show("Failed"); else MessageBox.Show("Success");
+                System.Diagnostics.Process.Start(Application.ExecutablePath);
+                Application.Exit();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ask = new OpenFileDialog();
+            if (ask.ShowDialog() == DialogResult.OK)
+            {
+                string filename = ask.FileName;
+
+                const int WM_FONTCHANGE = 0x001D;
+                const int HWND_BROADCAST = 0xffff;
+
+                int res = RemoveFontResource(filename);
+                SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
+                if (res == 0) MessageBox.Show("Failed"); else MessageBox.Show("Success");
+                System.Diagnostics.Process.Start(Application.ExecutablePath);
+                Application.Exit();
+            }
+        }
+    }
+}
